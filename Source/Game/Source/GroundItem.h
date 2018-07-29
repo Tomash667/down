206c8e1a1144553e9a80de1d9428ca641242ddaa
Tomashu <tomash4my@gmail.com>
2018-07-29 20:48:06 +0200

    removed old files

--- Source/Game/Source/GroundItem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct GroundItem
-{
-	SceneNode* node;
-	Item* item;
-	Vec3 pos;
-	float rot;
-};
--- Source/Game/Source/UnitStats.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameCore.h"
-#include "UnitStats.h"
-
-const UnitStats UnitStats::stats[2] = {
-	// walk	run		rot		alert	chase	can_run
-	2.5f,	7.f,	5.f,	10.f,	15.f,	true,	// human
-	1.5f,	0.f,	2.5f,	5.f,	10.f,	false,	// zombie
-};
